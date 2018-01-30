71ed0bcd3f716197045a1647e2702520bd6431c8
Maks <ffwmakssherbyna@gmail.com>
2018-01-31 00:45:34 +0200

    added migration for categories table

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+
+$route['migrate'] = 'migrate/index';
 $route['update'] = 'posts/update';
 $route['create'] = 'posts/create';
 $route['posts/(:any)'] = 'posts/view/$1';
